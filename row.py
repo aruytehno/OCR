ec7afb3ea16dd06d6f9ff2fdd3adc0faf2edf6c8
aruytehno <aruytehno@gmail.com>
2021-03-30 18:50:17 +0300

    Привёл код в читабельный вид. Убрал код отладки.

--- row.py
+++ row.py
@@ -22,60 +22,49 @@ def rotate_image(mat, angle):
     return rotated_mat
 
 
-# возвращает вложенный массив
-# [[((x,y),(w,h),angle)),((x,y),(w,h),angle)),...],[((x,y),(w,h),angle)),((x,y),(w,h),angle)),...],]
-def сортировка_контуров(контуры):
-    контуры2 = контуры.copy()
-    таблицаЯчеек = []
-    # print(len(контуры2))
-    # input()
-    while len(контуры2) > 0:
-        t = контуры2[0]  # первый попавшийся
-        y = контуры2[0][0][1]  # смотрим кординату y
-        строкаКонтуров = []  # здесь будут контрура принадлежащие одной строке
-        for _ in контуры2[:]:
-            # print(i)
-            # input()
+def sorting_contours(contours):
+    contours2 = contours.copy()
+    cell_table = []
+    while len(contours2) > 0:
+        t = contours2[0]  # первый попавшийся
+        y = contours2[0][0][1]  # смотрим кординату y
+        line_outlines = []  # здесь будут контрура принадлежащие одной строке
+        for _ in contours2[:]:
             nextY = _[0][1]
             if y + 5 > nextY > y - 5:
                 y = nextY
-                строкаКонтуров.append(_)
-                контуры2.remove(_)
-        таблицаЯчеек.append(строкаКонтуров)
-    return таблицаЯчеек
+                line_outlines.append(_)
+                contours2.remove(_)
+        cell_table.append(line_outlines)
+    return cell_table
 
 
-def показать_строку(таблицаТочек, n):
-    строка = таблицаТочек[n - 1]
+def show_row(таблицаТочек, n):
+    row = таблицаТочек[n - 1]
 
     maxY = 0
-    for _ in строка:
+    for _ in row:
         for __ in _:
             if __[1] > maxY:
                 maxY = __[1]
-    minY = строка[0][0][1]
-    for _ in строка:
+    minY = row[0][0][1]
+    for _ in row:
         for __ in _:
             if __[1] < minY:
                 minY = __[1]
 
     maxX = 0
-    for _ in строка:
+    for _ in row:
         for __ in _:
             if __[0] > maxX:
                 maxX = __[0]
-    minX = строка[0][0][0]
-    for _ in строка:
+    minX = row[0][0][0]
+    for _ in row:
         for __ in _:
             if __[0] < minX:
                 minX = __[0]
 
-    # print(maxY,minY,maxX,minX)
-    # input()
-
     crop_img = rotimg[int(minY):int(maxY), int(minX):int(maxX)]
-    # cv2.imshow("cropped", crop_img)
-    # cv2.waitKey(0)
     cv2.imwrite('out/cropped.png', crop_img)
 
 
@@ -85,7 +74,6 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(gray, 20, 255, cv2.THRESH_BINARY)
 img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
 
-# Get contours
 contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 output = img.copy()
@@ -93,7 +81,6 @@ output = img.copy()
 cnt = contours[1]
 ell = cv2.fitEllipse(cnt)
 angle = ell[2]
-# print(angle)
 rotimg = rotate_image(img, angle - 90)
 cv2.imshow("rotate", rotimg)
 roterode = rotate_image(img_erode, angle - 90)
@@ -103,54 +90,34 @@ contours, hierarchy = cv2.findContours(roterode, cv2.RETR_TREE, cv2.CHAIN_APPROX
 n = 0
 ячейки = []
 for idx, contour in enumerate(contours):
-    # (x, y, w, h) = cv2.boundingRect(contour)
-    # print("R", idx, x, y, w, h, cv2.contourArea(contour), hierarchy[0][idx])
-    # hierarchy[i][0]: the index of the next contour of the same level
-    # hierarchy[i][1]: the index of the previous contour of the same level
-    # hierarchy[i][2]: the index of the first child
-    # hierarchy[i][3]: the index of the parent
     if hierarchy[0][idx][3] == 1:
         n += 1
         rect = cv2.minAreaRect(contour)  # выход:((x,y),(w,h),angle))
-        # print(contour[0])
-        # input()
         ячейки.append(rect)
-        # print(rect)
-        # input()
         box = cv2.boxPoints(rect)
         box = np.int0(box)
-        # print(box)
-        # input()
         cv2.drawContours(rotimg, [box], 0, (250, 0, 0), 1)
 
-таблица = сортировка_контуров(ячейки)
-print(таблица[0])
-print("-----")
+table = sorting_contours(ячейки)
 
-таблицаТочек = []
-for _ in таблица:
-    строка = []
+point_table = []
+for _ in table:
+    row = []
     for __ in _:
-        # print(__)
-        # input()
-        строка.append(cv2.boxPoints(__))
-    таблицаТочек.append(строка)
+        row.append(cv2.boxPoints(__))
+    point_table.append(row)
 
-print(таблицаТочек[0])
-for _ in таблицаТочек:
+for _ in point_table:
     for __ in _:
         for ___ in __:
-            # print(___)
-            # input()
             cv2.circle(rotimg, (int(___[0]), int(___[1])), 2, (0, 0, 255), 2)
 
-показать_строку(таблицаТочек, 6)
+show_row(point_table, 6)
 
 n = 1
 d = 1
-for _ in таблица:
+for _ in table:
     for __ in _:
-        # cv2.circle(rotimg,(int(__[0][0]),int(__[0][1])) , 2, (0,0,255), 2)
         cv2.putText(rotimg, str(n), (int(__[0][0]), int(__[0][1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2,
                     cv2.LINE_AA)
         n += 1
