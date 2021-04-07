11c30fa82fe2387dddccf8004fb3aee4ed513557
aruytehno <aruytehno@gmail.com>
2021-04-07 09:45:03 +0300

    Перевёл комментарии

--- correct_skew.py
+++ correct_skew.py
@@ -2,51 +2,47 @@
 import os
 
 import numpy as np
-import argparse
 import cv2
 
-# construct the argument parse and parse the arguments
+# построить аргумент, синтаксический анализ и синтаксический анализ аргументов
 # ap = argparse.ArgumentParser()
 # ap.add_argument("-i", "--image", required=True,
 #                 help="path to input image file")
 # args = vars(ap.parse_args())
 # load the image from disk
-image = cv2.imread('examples' + os.sep + 'rotated' + os.sep + 'm21.png')
-# convert the image to grayscale and flip the foreground
-# and background to ensure foreground is now "white" and
-# the background is "black"
+image = cv2.imread('examples' + os.sep + 'rotated' + os.sep + 'example5cl+angle.png')
+# преобразовать изображение в оттенки серого и перевернуть передний план
+# и фон, чтобы убедиться, что передний план теперь "белый" и фон "черный"
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.bitwise_not(gray)
-# threshold the image, setting all foreground pixels to
-# 255 and all background pixels to 0
+# порог изображения, устанавливая для всех пикселей переднего плана значение
+# 255 и все пиксели фона на 0
 thresh = cv2.threshold(gray, 0, 255,
                        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-# grab the (x, y) coordinates of all pixel values that
-# are greater than zero, then use these coordinates to
-# compute a rotated bounding box that contains all
-# coordinates
+# получаем координаты (x, y) всех значений пикселей, которые
+# больше нуля, используйте эти координаты для
+# вычислить повернутую ограничивающую рамку, которая содержит все координаты
 coords = np.column_stack(np.where(thresh > 0))
 angle = cv2.minAreaRect(coords)[-1]
-# the `cv2.minAreaRect` function returns values in the
-# range [-90, 0); as the rectangle rotates clockwise the
-# returned angle trends to 0 -- in this special case we
-# need to add 90 degrees to the angle
+# функция `cv2.minAreaRect` возвращает значения в
+# диапазон [-90, 0); при вращении прямоугольника по часовой стрелке
+# вернули угловые тренды на 0 - в этом особом случае мы
+# нужно добавить 90 градусов к углу
 if angle < -45:
     angle = -(90 + angle)
-# otherwise, just take the inverse of the angle to make
-# it positive
+# в противном случае просто возьмите угол, обратный углу, чтобы сделать его положительным
 else:
     angle = -angle
-# rotate the image to deskew it
+# повернуть изображение, чтобы выровнять его
 (h, w) = image.shape[:2]
 center = (w // 2, h // 2)
 M = cv2.getRotationMatrix2D(center, angle, 1.0)
 rotated = cv2.warpAffine(image, M, (w, h),
                          flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-# draw the correction angle on the image so we can validate it
+# рисуем угол коррекции на изображении, чтобы мы могли его проверить
 cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle),
             (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-# show the output image
+# сохраняем выходное изображение
 print("[INFO] angle: {:.3f}".format(angle))
 # cv2.imshow("Input", image)
 # cv2.imshow("Rotated", rotated)
